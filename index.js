b87bf5f2c12b0d6e8c62ad3f50df20eaa0b09048
alejandro-baez <89477369+alejandro-baez@users.noreply.github.com>
2023-08-01 16:11:55 -0400

    Completed workshop (#1)

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY);
+
+
+
+async function getImage(query){
+    const endpoint =
+      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+   
+      try{
+        const response = await fetch(endpoint)
+        //console.log(response);
+        //.json() is used for objs, JSON.parse() is for strings
+        const parsedRes =await response.json();
+       
+        console.log(parsedRes.data[0].url)
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+getImage("cat")
